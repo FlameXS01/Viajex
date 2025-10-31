76729d4854c5878c5520b89e52f3586ffb46f435
Jailer PC <perezjailer41@gmail.com>
2025-10-31 10:22:58 -0400

    add requests and department models

--- infrastructure/database/models.py
+++ infrastructure/database/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from infrastructure.database.session import Base
 
 class UserModel(Base):
@@ -19,3 +20,33 @@ class UserModel(Base):
 
     def __repr__(self):
         return f"<UserModel(id={self.id}, username='{self.username}')>"
+
+    
+class RequestUserModel(Base):
+    """
+    Modelo de SQLAlchemy para la tabla request.
+    Representa la estructura de la tabla Solicitantes en la base de datos
+    """
+    __tablename__ = "requests"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    fullname = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    ci = Column(String(15), unique=True, nullable=False)
+    department_id = Column(Integer, ForeignKey("department.id"), nullable=False)
+
+    department = relationship("DepartmentModel", back_populates="requests")
+    
+class DepartmentModel(Base):
+    """
+    Modelo de SQLAlchemy para la tabla department.
+    Representa la estructura de la tabla Departamentos en la base de datos
+    """
+    __tablename__ = "department"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, index=True, nullable=False)
+    
+    requests = relationship("RequestUserModel", back_populates="department")
+    
